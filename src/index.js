25e134683f3004cb7f98f4ad81688b6dc89067b0
Coder <31937370+Code1441@users.noreply.github.com>
2018-02-23 01:17:37 +0300

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   var brackets = {};
+   var openBracketsArray = [];
+   var closeBracketsArray = []
+   for(var i = 0; i < bracketsConfig.length; i++){
+       brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+       closeBracketsArray.push(bracketsConfig[i][1]);
+       openBracketsArray.push(bracketsConfig[i][0]);
+   }
+    var str = str.split('');
+    var stack = [];
+    for(var i =0; i< str.length; i++){
+        if(openBracketsArray.indexOf(str[i])!=-1){
+         for(openBracket in brackets) {
+          if (str[i] == openBracket) {
+              stack.push(brackets[openBracket]);
+              break;
+          }
+      }}
+     else if (stack.length == 0 || stack.pop()!==str[i] && stack.pop()!=='|') return false;
+    };
+    stack = stack.filter(function(el){
+        if (el!="|") return true;
+    })
+    if(stack.length==0) return true;
+    else return false;
 }
