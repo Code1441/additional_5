71cfe4b26b14790bdb288c62c64e99043e3ef4b5
TatsianaGelahova <tgelahova99@gmail.com>
2018-02-25 10:41:31 +0200

    fix: working code with the same parentheses

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-   var brackets = {};
-   var openBracketsArray = [];
-   var closeBracketsArray = []
-   for(var i = 0; i < bracketsConfig.length; i++){
-       brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
-       closeBracketsArray.push(bracketsConfig[i][1]);
-       openBracketsArray.push(bracketsConfig[i][0]);
-   }
+    var brackets = {};
+    var openBracketsArray = [];
+    var closeBracketsArray = [];
+    var similarBrackets = [];
+
+    for(var i = 0; i < bracketsConfig.length; i++){
+        if(bracketsConfig[i][0] == bracketsConfig[i][1]){
+            similarBrackets.push(bracketsConfig[i][1]);
+        }
+        brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+        closeBracketsArray.push(bracketsConfig[i][1]);
+        openBracketsArray.push(bracketsConfig[i][0]);
+    }
     var str = str.split('');
     var stack = [];
     for(var i =0; i< str.length; i++){
         if(openBracketsArray.indexOf(str[i])!=-1){
-         for(openBracket in brackets) {
-          if (str[i] == openBracket) {
-              stack.push(brackets[openBracket]);
-              break;
-          }
-      }}
-     else if (stack.length == 0 || stack.pop()!==str[i] && stack.pop()!=='|') return false;
+            for(openBracket in brackets) {
+                if (str[i] == openBracket) {
+                    if(similarBrackets.indexOf(str[i])!==-1 && stack.indexOf(str[i])!==-1){
+                        stack.pop();
+                        break;
+                    }
+                    stack.push(brackets[openBracket]);
+                    break;
+                }
+            }}
+        else if (stack.length == 0 || stack.pop()!==str[i] && stack.pop()!=='|') return false;
     };
     stack = stack.filter(function(el){
         if (el!="|") return true;
@@ -25,3 +34,4 @@ module.exports = function check(str, bracketsConfig) {
     if(stack.length==0) return true;
     else return false;
 }
+
